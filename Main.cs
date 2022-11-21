a46c62990b12d4fee3a638c4e6418ecf35b00a66
Drovyng <DrovyngOfficial@gmail.com>
2022-11-21 21:07:20 +0300

    v0.0.2

---
+++ Main.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Athormito
+{
+    public class Main : Game
+    {
+        public static Main instance;
+
+        public const double UpdateTime = 1d / 60d;
+        public double timeToUpdate;
+        public double timeToDraw;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Main()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            instance = this;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+        public void EngineUpdate()
+        {
+
+        }
+        public void EngineDraw()
+        {
+            GraphicsDevice.Clear(Color.Gray);
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            timeToUpdate += gameTime.ElapsedGameTime.Seconds;
+            if (timeToUpdate >= UpdateTime)
+            {
+                EngineUpdate();
+                timeToUpdate = 0;
+            }
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            timeToDraw += gameTime.ElapsedGameTime.Seconds;
+            if (timeToDraw >= UpdateTime)
+            {
+                EngineDraw();
+                timeToDraw = 0;
+            }
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Scripts/Engine/EngObject.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Athormito.Scripts.Engine
+{
+    public class EngObject
+    {
+        public string id;
+
+        public string texture;
+
+        public bool animated;
+        public int animIndex;
+
+        public Vector2 position;
+        public Vector2 size;
+
+        public virtual void Create()
+        {
+
+        }
+        public virtual void Update()
+        {
+
+        }
+    }
+}
